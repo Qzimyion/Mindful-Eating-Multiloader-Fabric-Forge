8e3dfc7a197778ae0d968ac63bde7aea73dcb0fc
Qzimyion <samyakchauhan676@gmail.com>
2024-09-22 03:40:25 +0530

    Initial Commit

--- src/main/java/com/minecraftabnormals/mindful_eating/client/ClientSetup.java
+++
@@ -1,9 +0,0 @@
-package com.minecraftabnormals.mindful_eating.client;
-
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-
-public class ClientSetup {
-    public static void init(FMLClientSetupEvent event) {
-        HungerOverlay.init();
-    }
-}
